ea1699afcd8164baad0e98dfd9baf060576f83ae
Nilson Reyes Sigua <nilsonreyess@hotmail.es>
2024-05-23 20:26:04 -0500

    First commit

---
+++ src/tests/tests.java
@@ -0,0 +1,14 @@
+
+package tests;
+
+import views.FrmIntro;
+
+public class tests {
+
+    public static void main(String[] args) {
+        FrmIntro frmIntro = new FrmIntro();
+        frmIntro.setVisible(true);
+        frmIntro.setLocationRelativeTo(null);
+    }
+    
+}
